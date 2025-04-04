6229b4dfafbe69603fa25406fad5fff33c4afae8
Francisco Arancibia <frarancibia.g@gmail.com>
2025-04-04 14:18:38 -0300

    "Consumiendo la api de la tienda"

---
+++ frontend/src/services/store.api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:8000/api/store/";
+
+export const api = axios.create({
+  baseURL: baseURL,
+  headers: { "Content-Type": "application/json" },
+});
